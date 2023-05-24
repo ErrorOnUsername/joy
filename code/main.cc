7fbcd0670da7160b546eeefa6e91ea399266574a
ErrorOnUsername <richarddavidson000@gmail.com>
2023-05-23 23:01:17 -0700

    init: handle bad main module

--- code/main.cc
+++ code/main.cc
@@ -1,10 +1,13 @@
 #include "compiler.hh"
+#include "log.hh"
 #include "profiling.hh"
 
 int main()
 {
 	PROF_START_APP( "swarm" );
 	TIME_MAIN();
+
+	Module* main_module;
 
 	bool exited_with_error;
 	{
@@ -13,10 +16,15 @@ int main()
 		int worker_count = 8;
 		Compiler_JobSystem_Start( worker_count );
 
-		Compiler_ScheduleLoad( "./test_files/test.df" );
+		main_module = Compiler_ScheduleLoad( "./test_files/test.df" );
 
 		while ( Compiler_JobSystem_IsBusy() );
-		exited_with_error = !Compiler_JobSystem_Terminate();
+		exited_with_error = !Compiler_JobSystem_Terminate() || !main_module;
+	}
+
+	if ( !main_module )
+	{
+		log_error( "Couldn't load the main module at path '%s'", "./test_files/test.df" );
 	}
 
 	char const* status_str = "\x1b[32;1msucceeded\x1b[0m";
